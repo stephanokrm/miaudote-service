647594863d8480c446dd88949d8c0607fe3dc9a4
Stephano Ramos <stephano.ramos.p@gmail.com>
2022-10-25 12:47:50 -0300

    refactors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
@@ -22,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->isProduction()) {
+            URL::forceScheme('https');
+        }
+
         Password::defaults(function () {
             $rule = Password::min(8);
 
